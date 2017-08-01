112388213c263b3f460452247ba4f7d14e2840e5
Masayuki Higashino <msyk@hgsn.info>
2017-08-02 01:17:32 +0900

    bundle gem notebook-cat --bin --mit --test=minitest --no-coc

---
+++ lib/notebook/cat.rb
@@ -0,0 +1,7 @@
+require "notebook/cat/version"
+
+module Notebook
+  module Cat
+    # Your code goes here...
+  end
+end
---
+++ lib/notebook/cat/version.rb
@@ -0,0 +1,5 @@
+module Notebook
+  module Cat
+    VERSION = "0.1.0"
+  end
+end
